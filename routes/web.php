11ebe2f1d9c70c1ff096cafb42d63ff53863c866
2702徐愷斌 <d1084182702@gm.lhu.edu.tw>
2021-12-01 11:58:46 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "welcome2";
 });
+
+
+
+Route::resource( "coins", CoinsController::class);
+Route::resource( "chains", ChainsController::class);
